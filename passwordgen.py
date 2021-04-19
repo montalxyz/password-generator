b0f306bd4c56905eed4a9b45d1a7fb3705c1db85
montal <8894496+montalxyz@users.noreply.github.com>
2021-04-19 13:18:34 +0200

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,24 +1,21 @@
 import string
 import random
 
-
+#ask user for length of passwords
 lpasswords = input('how long should the passwords be?')
+#ask user for how many passwords to generate
 npasswords = input('how many passwrords should be generated?')
 
-
-
-
+#retrives all possible characters
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 num = string.digits
 symbols = string.punctuation
 
-
+#aggregate all into the same pool
 all = lower + upper + num + symbols
 
-
-
-
+#loop that generates based on the user's request
 while npasswords > 0:
     new = random.sample(all,lpasswords)
     npasswords = npasswords -1
